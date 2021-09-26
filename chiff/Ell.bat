49c853f84cb2b9f296cee628a7ef1047309a9489
TheHigherUps <classified@thehigherups.org>
2021-09-25 22:49:01 -0400

    Add files via upload

---
+++ chiff/Ell.bat
@@ -0,0 +1,30 @@
+@echo off
+:loop
+set /a num=(%Random% %%9)+1
+color %num%
+echo       _____          
+echo      |\    \         
+echo      |:\____\        
+echo      |::|   |        
+echo      |::|   |        
+echo      |::|   |        
+echo      |::|   |        
+echo      |::|   |        
+echo      |::|___|______  
+echo      /::::::::\    \ 
+echo     /::::::::::\____\
+echo    /:::/----/      
+echo   /:::/    /         
+echo  /:::/    /          
+echo /:::/    /           
+echo \::/    /            
+echo  \/____/             
+echo                      
+                     
+                     
+                     
+                     
+                                    
+                                                                                                                
+pause
+goto :loop
